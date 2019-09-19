d0f6031a14c56cb23ea00e667e326d04ce75e719
jkotheimer <jkotheimer@luc.edu>
2019-09-19 14:30:28 -0500

    Created fresh Maven hello world project

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Chromium_Titanium_Pigeon_Sticks
+
+## Compile, Test, & Run
+
+- mvn clean compile
+- mvn test
+- mvn exec:java
---
+++ src/main/java/CTPS/puzzlesolver/App.java
@@ -0,0 +1,11 @@
+package CTPS.puzzlesolver;
+
+/**
+ * Hello Chromium Titanium Pigeon Sticks!
+ *
+ */
+public class App {
+	public static void main( String[] args ) {
+        System.out.println( "Hello, World!" );
+    }
+}
