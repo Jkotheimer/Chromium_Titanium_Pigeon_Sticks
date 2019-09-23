bde215bd5066c5aa88b1b3adc9e3d2aa8371240a
jkotheimer <jkotheimer@luc.edu>
2019-09-23 11:59:10 -0500

    Object mapper works - we have a Java object with all of the data in it

--- src/main/java/CTPS/puzzlesolver/App.java
+++ src/main/java/CTPS/puzzlesolver/App.java
@@ -1,11 +1,22 @@
 package CTPS.puzzlesolver;
 
+import java.io.File;
+import java.io.IOException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.JsonNode;
+
 /**
  * Hello Chromium Titanium Pigeon Sticks!
  *
  */
 public class App {
 	public static void main( String[] args ) {
-        System.out.println( "Hello, World!" );
+		ObjectMapper objectMapper = new ObjectMapper();
+		try {
+			// Read the entire JSON file of data into a single json node to be sorted later
+			JsonNode jsonNode = objectMapper.readTree(new File("test_cases/Mixed_Up_Purses.json"));
+			String names = jsonNode.get("people").get(0).asText();
+			System.out.println("Success: " + names);
+		} catch(IOException e) { System.err.println(e); }
     }
 }
