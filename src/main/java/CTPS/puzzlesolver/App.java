38389891673861f947b6e65b4032860e2b9688c3
jkotheimer <jkotheimer@luc.edu>
2019-10-29 14:41:19 -0500

    Reconfigured Main to accept the file name from stdin rather than having MUP hardcoded

--- src/main/java/CTPS/puzzlesolver/App.java
+++ src/main/java/CTPS/puzzlesolver/App.java
@@ -1,18 +1,26 @@
 package CTPS.puzzlesolver;
 
+import java.util.Scanner;
+
 /**
  * Hello Chromium Titanium Pigeon Sticks!
  *
  */
 public class App {
 	public static void main( String[] args ) {
-		ClueMap clues = new ClueMap("test_cases/Mixed_Up_Purses.json");
-		PuzzleSolver solver = new PuzzleSolver(new ReferenceMap("test_cases/Mixed_Up_Purses.json"));
+		Scanner in = new Scanner(System.in);
+		String file;
+		if(args.length == 0) {
+			System.out.print("No input file specified!\nEnter a valid puzzle JSON file: ");
+			file = in.nextLine();
+		} else file = args[0];
+		ClueMap clues = new ClueMap(file);
+		PuzzleSolver solver = new PuzzleSolver(new ReferenceMap(file));
 		
 		// Iterate through the clues and pass clues to the reference map for solving
 		// Then call the autoEliminate function to make the reference map cross reference solvable items
 		// Do this until the map is solved
-		System.out.println("Solving the puzzle located at" + "test_cases/Mixed_Up_Purses.json" );
+		System.out.println("Solving the puzzle located at" + file );
 		int counter = 0;
 		while(!solver.isSolved()) {
 			while(clues.hasNext())
